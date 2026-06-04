0e4d08687a9a3e17d786357ab7048e522841718d
Nanobind <141485551+Nanobind@users.noreply.github.com>
2026-06-04 14:54:56 +0930

    Add files via upload

---
+++ LOB-stage2.py
@@ -0,0 +1,28 @@
+randoom = "5232109503833121332712131424220923" #random string to be removed 
+
+f = open("LOB/filewithsecret.txt", "r")
+
+secret = (f.read()).replace(str(randoom), " ") #replaces every instance of randoom with a space 
+
+secret_vault = []
+
+Secret = secret.split(" ") # splits in each individual number 
+ 
+for i in range(0,3):
+    
+    secret2 = [int(x) for x in Secret]
+
+    for x in secret2: #increments over every value of x
+                    
+        secret_vaulted = str(chr(x)) #turns integer to ascii letter equiv
+        secret_vault.append(secret_vaulted) #appends to the array 
+        completedmessage = "".join(secret_vault) #joins all in one string 
+        Secret = completedmessage.split(" ") #splits apart into list
+        print(Secret)
+        Secret.pop()        
+        
+    if i != 2:
+        secret_vault.clear()
+    else:
+        print(completedmessage)
+ 
---
+++ secretcreator.py
@@ -0,0 +1,49 @@
+import secrets 
+
+secretGenerator = secrets.SystemRandom()
+
+random_seperation = [] #table of the random seperation code 
+
+random_seperator_range = secretGenerator.randint(8,30) #length of the random seperation between values
+
+for i in range(random_seperator_range):
+    random_seperator = secretGenerator.randint(0,50)
+    random_seperation.append(random_seperator)
+    
+randoom = [str(item) for item in random_seperation] #random string to differentiate between each letter 
+
+
+User_input = input("Input the secret message: ")
+
+secret2 = [ord(x) for x in User_input] #turns the input into ascii values 
+    
+for i in range(0,2): 
+        
+    secretised = ' '.join([str(item) for item in secret2]) #takes the ascii values joins together 
+    secret3 = [str(x) for x in secretised] 
+    secret2 = [ord(x) for x in secret3] #converts further via ascii 
+    secret2.append(32)
+    
+passwordtranslated = str(secret2).translate({ord(i): None for i in '[],'}) #removes the unnessecary characters to be translated back to a message 
+randoomy = str(randoom).translate({ord(i): None for i in "[],' "})
+
+print(secret2) # each letter in ascii encoded 3 times
+
+furtherENCODE = str(passwordtranslated).replace(" ",str(randoomy))
+
+print(passwordtranslated) #final encoded message with string inbetween 
+
+
+
+with open("LOB/filewithsecret.txt", "w")as file: 
+
+    file.writelines(furtherENCODE) #write the encoded message to filewithsecret 
+    
+with open("LOB/LOB-stage2.py", "r")as file: #reads the first line to be replaced 
+    
+    data = file.readlines()
+    
+data[0] = f'randoom = "{randoomy}"\n' #writes the first line of stage 2 with the random section that needs to be removed 
+
+with open("LOB/LOB-stage2.py", "w")as file:
+    file.writelines(data)
